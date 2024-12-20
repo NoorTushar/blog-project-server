a02256f1daa38e4d6fd4c39742b65868df392e42
NoorTushar <tushar.noor.khan@gmail.com>
2024-12-20 23:18:28 +0600

    blog search ok

---
+++ src/app/modules/blog/blog.constants.ts
@@ -0,0 +1,1 @@
+export const blogSearchTerms = ["title", "content"] as const;
--- src/app/modules/blog/blog.service.ts
+++ src/app/modules/blog/blog.service.ts
@@ -1,3 +1,4 @@
+import { blogSearchTerms } from "./blog.constants";
 import { TBlog } from "./blog.interface";
 import { BlogModel } from "./blog.model";
 
@@ -6,8 +7,28 @@ const createBlogIntoDB = async (payload: TBlog) => {
    return result;
 };
 
-const getAllBlogsFromDB = async () => {
-   const result = await BlogModel.find().populate("author");
+const getAllBlogsFromDB = async (query: Record<string, unknown>) => {
+   console.log(query);
+
+   const search = query?.search || "";
+
+   //    const result = await BlogModel.find({
+   //       $or: [
+   //          { title: { $regex: search, $options: "i" } },
+   //          { content: { $regex: search, $options: "i" } },
+   //       ],
+   //    }).populate("author");
+
+   const searchQuery = BlogModel.find({
+      $or: blogSearchTerms.map((term) => {
+         return {
+            [term]: { $regex: search, $options: "i" },
+         };
+      }),
+   }).populate("author");
+
+   const result = await searchQuery;
+
    return result;
 };
 
