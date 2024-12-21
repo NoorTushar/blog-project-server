cdf60b65b25b3ea652bbc07b08961571bd8559be
NoorTushar <tushar.noor.khan@gmail.com>
2024-12-21 20:45:17 +0600

    Zod validation Error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
-import express, { Response, Request, Application } from "express";
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { Response, Request, Application, NextFunction } from "express";
 import cors from "cors";
 import notFound from "./app/middlewares/notFound";
 import router from "./app/routes";
+import httpStatus from "http-status";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import { ZodError, ZodIssue } from "zod";
 
 const app: Application = express();
 
@@ -15,6 +21,53 @@ app.use("/api", router);
 
 app.get("/", (req: Request, res: Response) => {
    res.send("Hello From the Blog Site ⚡️⚡️");
+});
+
+// app.use(globalErrorHandler);
+
+app.use((err: any, req: Request, res: Response, next) => {
+   let statusCode = err.statusCode || 400;
+   let message = err.message || "something went wrong";
+   let error = {
+      details: {
+         path: "",
+         message: err?.message || "Something went wrong",
+      },
+   };
+
+   const handleZodError = (err: ZodError) => {
+      const statusCode = 400;
+      const message = "Validation Error";
+      const error = {
+         details: err.issues.map((issue: ZodIssue) => {
+            return {
+               path: issue.path[issue.path.length - 1],
+               message: issue.message,
+            };
+         }),
+      };
+      return {
+         statusCode,
+         message,
+         error,
+      };
+   };
+
+   if (err instanceof ZodError) {
+      const simplifiedError = handleZodError(err);
+      statusCode = simplifiedError.statusCode;
+      message = simplifiedError.message;
+      error = simplifiedError.error;
+   }
+   //ultimate return
+   return res.status(err?.statusCode || httpStatus.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message,
+      statusCode,
+      error,
+      amarError: err,
+      stack: err?.stack || null,
+   });
 });
 
 //Not Found
